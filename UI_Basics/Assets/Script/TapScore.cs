2ef3cda52680f2b887caf50b406841a0eab19614
zoulord <zt421918@ohio.edu>
2020-04-29 15:59:16 -0400

    updated mbuh

--- UI_Basics/Assets/Script/TapScore.cs
+++ UI_Basics/Assets/Script/TapScore.cs
@@ -5,7 +5,13 @@ using UnityEngine.UI;
 public class TapScore : MonoBehaviour {
 
     public Text tapScoreText;
+    public Text tenTapText;
     public int tapScore;
+    public GameObject tenTaps;
+     int score;
+     int oldScore;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -16,10 +22,25 @@ public class TapScore : MonoBehaviour {
 	void Update () {
 
         tapScoreText.text = "Score: " + tapScore;
+       
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             tapScore++;
+            
+            // Debug.Log("10 taps");
         }
+
+         if (tapScore == oldScore + 10)
+ {
+
+    
+
+     Instantiate(tenTaps);
+     oldScore = tapScore;
+     tenTaps.SetActive(true);
+     
+ }
+ 
 	}
 }
