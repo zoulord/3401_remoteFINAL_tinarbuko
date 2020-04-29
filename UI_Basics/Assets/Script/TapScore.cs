fe7826e07824046ea0e3c239f3eaa7bada259e97
zoulord <zt421918@ohio.edu>
2020-04-29 11:22:39 -0400

    yeah updated the tapscore!!

---
+++ UI_Basics/Assets/Script/TapScore.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TapScore : MonoBehaviour {
+
+    public Text tapScoreText;
+    public int tapScore;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        tapScoreText.text = "Score: " + tapScore;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            tapScore++;
+        }
+	}
+}
